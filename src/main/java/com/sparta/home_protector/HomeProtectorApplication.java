3d44fb1247ed5e297f9379222eeb4cd7f3a1848a
9yuhyeon <kkh2742@gmail.com>
2023-07-14 17:42:42 +0900

    init commit

---
+++ src/main/java/com/sparta/home_protector/HomeProtectorApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.home_protector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeProtectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeProtectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/home_protector/HomeProtectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.home_protector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeProtectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
