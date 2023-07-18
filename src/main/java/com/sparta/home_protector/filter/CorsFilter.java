2c440cf3026e799cd48df0e8248b6390eb164633
algorijun <123007169+HGive@users.noreply.github.com>
2023-07-19 00:48:58 +0900

    cors

--- src/main/java/com/sparta/home_protector/filter/CorsFilter.java
+++ src/main/java/com/sparta/home_protector/filter/CorsFilter.java
@@ -22,7 +22,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+                "*");
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
