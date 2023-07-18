bfc5697b122249d273158d703dfc7599f75b571f
9yuhyeon <kkh2742@gmail.com>
2023-07-18 18:05:02 +0900

    Temp : 게시글 조회 시 createAt, updateAt 필드 추가

--- src/main/java/com/sparta/home_protector/dto/PostResponseDto.java
+++ src/main/java/com/sparta/home_protector/dto/PostResponseDto.java
@@ -2,8 +2,10 @@ package com.sparta.home_protector.dto;
 
 import com.sparta.home_protector.entity.Comment;
 import com.sparta.home_protector.entity.Post;
+import com.sparta.home_protector.entity.Timestamped;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Getter
@@ -15,6 +17,8 @@ public class PostResponseDto {
     private List<String> images;
     private int viewCount;
     private List<Comment> comments;
+    private LocalDateTime createdAt;
+    private LocalDateTime updateadAt;
 
     public PostResponseDto(Post post) {
         this.id = post.getId();
@@ -24,5 +28,7 @@ public class PostResponseDto {
         this.images = new ArrayList<>(post.getImages().values());
         this.viewCount = post.getViewCount();
         this.comments = post.getCommentList();
+        this.createdAt = post.getCreatedAt();
+        this.updateadAt = post.getUpdatedAt();
     }
 }
